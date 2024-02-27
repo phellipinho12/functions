27bd058320ec6b91875078bff998dab6058b0177
phellipinho12 <phellipe.lpereira@gmail.com>
2024-02-27 12:23:06 -0300

    funcao basica

---
+++ main.py
@@ -0,0 +1,10 @@
+#valor_01 = 3
+#valor_02 = 5
+#print(valor_01 + valor_02)
+
+def minha_soma(valor_01:int,valor_02:int) -> int:
+    valor_03 = valor_01 + valor_02
+    return valor_03
+
+print(minha_soma(1,2))
+print(minha_soma(2,2))
